08fb4053fe3b16cd2e0a59158d7dbdaa6fbbea51
Jeongp4939 <jeongp12@gmail.com>
2021-07-12 16:03:54 +0900

    7/12 class project2_03

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = 'accountapp'
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+
+    path('create/', AccountCreateView.as_view(), name='create')    # as_view 클래스가 아닌 함수로서 가져와줌
 ]
