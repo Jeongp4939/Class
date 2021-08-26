0dec65a8a2e57853ed27a1eb7fe8122d38d5c83d
jeong <jeongp12@gmail.com>
2021-08-26 15:48:50 +0900

    08/26 class project2 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -11,26 +11,7 @@ from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountCreationForm
-from accountapp.models import HelloWorld
 from articleapp.models import Article
-
-
-@login_required
-def hello_world(request):
-    if request.method == 'POST':  # POST CREATE DATA
-
-        temp = request.POST.get('hello_world_input')
-
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
-
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))  # accountapp 의 hello_world 라는 앱으로 가라는 뜻
-
-    else:
-        hello_world_list = HelloWorld.objects.all()  # sql의 셀렉트문 # GET VIEW DATA
-        return render(request, 'accountapp/hello_world.html',
-                      context={'hello_world_list': hello_world_list})
 
 
 class AccountCreateView(CreateView):
@@ -75,5 +56,5 @@ class AccountUpdateView(UpdateView):
 class AccountDeleteView(DeleteView):  # 탈퇴시엔 form이 필요 없으므로 일단 제외함
     model = User
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/delete.html'
